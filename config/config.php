f2162e7b473317bd50d8084471681668fc89221f
felicianofig <69013356+felicianofig@users.noreply.github.com>
2024-05-19 11:32:06 -0700

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+date_default_timezone_set("America/Hermosillo");
+define('URL', 'http://localhost/loca/');
+
+define('HOST', 'localhost');
+define('DB', 'tacostacos');
+define('USER', 'root');
+define('PASSWORD', "");
+define('CHARSET', 'utf8mb4');
+
+?>
+
